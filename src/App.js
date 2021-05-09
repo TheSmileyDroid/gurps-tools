c7351f9a1ebf249c3bcc65bf55355552c9c23640
Gabriel Henrique Silva <ghsilha@gmail.com>
2021-05-09 12:28:09 -0300

    tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <div>
+            <nav>
+              <ul className="magic-types">
+                <li>
+                  <Link className="App-link" to="/Mana">Mana</Link>
+                </li>
+                <li>
+                  <Link className="App-link" to="/Bruxa">Bruxa</Link>
+                </li>
+                <li>
+                  <Link className="App-link" to="/Animfluo">Animfluo</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/Mana">
+                <Mana />
+              </Route>
+              <Route path="/Bruxa">
+                <Bruxa />
+              </Route>
+              <Route path="/Animfluo">
+                <Animfluo />
+              </Route>
+              <Route path="/">
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
+}
+
+function Mana() {
+  return (
+    <div>
+      <h2>Mana</h2>
+    </div>
+  );
+}
+
+function Bruxa() {
+  return (<div><h2>Bruxa</h2></div>);
+}
+
+function Animfluo() {
+  return (<div><h2>Animfluo</h2></div>);
 }
 
 export default App;
